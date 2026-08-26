9b8dfd830793f7b2c97f99d1a6f71a1ce2601bd3
dai9u9r3jTienOofwm <ti1306en6002@gmail.com>
2026-08-26 15:46:38 +0700

    Step 21: apply_attention_weights_to_values

--- model.py
+++ model.py
@@ -205,8 +205,13 @@ def softmax_attention_weights(masked_scores):
     # TODO: softmax over the last axis, zeroing rows that are entirely -inf
     return torch.where(F.softmax(masked_scores,-1).isnan(),0.0,F.softmax(masked_scores,-1))
 
-# Step 21 - apply_attention_weights_to_values (not yet solved)
-# TODO: implement
+# Step 21 - apply_attention_weights_to_values
+import torch
+
+def apply_attention_weights_to_values(attention_weights, value):
+    """Multiply attention weights by the value matrix to produce context vectors."""
+    # TODO: combine attention weights (..., Lq, Lk) with value (..., Lk, d_v)
+    return torch.matmul(attention_weights,value)
 
 # Step 22 - scaled_dot_product_attention (not yet solved)
 # TODO: implement
